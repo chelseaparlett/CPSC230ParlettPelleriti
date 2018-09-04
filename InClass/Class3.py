2e4d79043b3b50baef777be0d731ec998e39fe7d
cmparlettpelleriti <parlett@chapman.edu>
2018-09-03 20:18:23 -0700

    C3

---
+++ InClass/Class3.py
@@ -0,0 +1,60 @@
+#  PRINTING AND INPUT
+
+print("I hate sand.")
+
+adj1 = input()
+adj2 = input()
+adj3 = input()
+
+print("it's "  + adj1 + " and "  + adj2 + " and " + adj3 + ". And  it gets everywhere.")
+#--------------------------------------------
+# # TYPES
+#
+# a = 7
+#
+# duck = "too old to be a duckling, quack, quack."
+#
+#
+#
+# print(type(a))
+# print(type(duck))
+#
+#
+# a = str(a)
+#
+# a + duck
+# print(type(a))
+#---------------------------------------------
+# #What Type will these be?
+# my_boolean = True
+# c = 9
+# s = "can a bee be said to be or not to be an entire bee"
+# fpn = 7.892
+#---------------------------------------------
+# # OPERATORS
+#
+# print(1 +  1)
+#
+#
+# print("I'm " + "Hungry")
+#
+# #what's the difference here?
+#---------------------------------------------
+# 5*9
+# "hello" * 9
+# string_var =  "rabbit"
+#
+# num_var = 8
+#
+#
+# print(string_var * 3)
+#
+# print(num_var * 3)
+#---------------------------------------------
+# DIVISION
+# 
+# print(10/2)
+#
+# print(10//2)
+#
+# print(10%2)
