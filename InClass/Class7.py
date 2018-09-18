133c94fafe3098aa8ab5650dca1391769ad46c95
cmparlettpelleriti <parlett@chapman.edu>
2018-09-18 11:26:54 -0700

    7

---
+++ InClass/Class7.py
@@ -0,0 +1,23 @@
+#I HATE E's!
+# ask the user to input a string, print all the letters
+# except "e" using continue. What would happen if I really hated
+# e's and wanted to stop printing if I found one?
+
+#Perfect number
+#Perfect numbers occur when the sum
+#of a number's divisors (except itself), equals that number
+#e.g. 6 = 1 + 2 + 3
+num = int(input("Give me an int:"))
+divSum = 0
+
+for n in range(1,num):
+    if num%n == 0:
+        divSum += n
+if.....?
+
+# Use if/elif/else to do abundant and deficient
+#use a while loop to keep prompting user until
+#they give a perfect num (use sentinel loop)
+
+
+#PLAY PIG!
