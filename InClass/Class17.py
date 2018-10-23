5544c8d87d87b739671035d7400c7193e7335768
cmparlettpelleriti <parlett@chapman.edu>
2018-10-23 11:52:21 -0700

    17done

--- InClass/Class17.py
+++ InClass/Class17.py
@@ -1,28 +1,39 @@
 # Function to check length of words
-def listStringCheck(L):
-    odds = []
-    for i in L:
-        if len(i)%2 ==1:
-            odds.append(i)
-    return odds
+# def listStringCheck(L):
+#     odds = []
+#     for i in L:
+#         if len(i)%2 ==1:
+#             odds.append(i)
+#     return odds
 
-k = ["List", "item"]
+def lSC(L):
+    odds = []
+    for word in L:
+        if len(word)%2 == 1:
+            odds.append(word)
+    return odds
+k = ["List", "items"]
 
 odds = listStringCheck(k)
 
 print(odds)
 
-a = 7
-a = "cloud"
 #-----------------------------
 # Function to remove vowels
-def vowelRemover(st):
+# def vowelRemover(st):
+#     for v in "aeiou":
+#         st = st.replace(v,"")
+#     return st
+
+def vR(st):
+    st = st.lower()
     for v in "aeiou":
-        st = st.replace(v,"")
+        if v in st:
+            st = st.replace(v,"")
     return st
 
 
-novs = vowelRemover("elephants")
+novs = vR("elephants")
 print(novs)
 #why does nothing print?
 #-----------------------------
