0b36fcbd52045ab7bce99018c62b3d3bcd9b720e
cmparlettpelleriti <parlett@chapman.edu>
2018-11-08 11:23:37 -0800

    22

---
+++ InClass/Class22.py
@@ -0,0 +1,11 @@
+#Dictionaries (How are they indexed? How do we create them?
+# How do we add things to them? replace things? grab values?
+# keys?)
+#Functions (how do we define one? Why do we use them?
+# what are default arguments and how do you define them?
+# how to call a function.)
+#File (how to open, close, and what mode to use?)
+
+#OLD STUFF----------------------------
+#For/while
+#Strings, lists...
