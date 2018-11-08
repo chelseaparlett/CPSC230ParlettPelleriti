167bd0365f966e7d9796d213403cc0f1847cff07
cmparlettpelleriti <parlett@chapman.edu>
2018-11-07 22:52:10 -0800

    Practice2

--- InClass/Class21.py
+++ InClass/Class21.py
@@ -1,14 +1,15 @@
-def saleMaker(item, percent, inventory):
+def saleMaker(item = "apples", percent = 0.25, inventory = {"apples": 10.5}):
     inventory[item] = round(percent* inventory[item],2)
+    print(inventory)
     percent = percent + 0.2 #get rid of stock
-    # j = 1
+    return percent
 
-i = {"apples": 2.45, "plumbs": 10.10, "almonds": 100.25}
+#i = {"apples": 2.45, "plumbs": 10.10, "almonds": 100.25}
 
 nprice = .5
-print("BEFORE:",i, nprice)
-saleMaker("apples",nprice,i)
-print("AFTER:",i,nprice)
+#print("BEFORE:",i, nprice)
+saleMaker()
+#print("AFTER:",i,nprice)
 #How can I change my code to automatically take 25% as the default price?
 # How about the default item and dict are "apples" and {"apples": 10.5}
 
@@ -17,3 +18,12 @@ print("AFTER:",i,nprice)
 #--------------------------------------------------
 # Write a function prime, that has the default argument 100
 # Call it using the default arguments, call it using 10
+def prime(n = 100):
+    p = True
+    for x in range(2,n):
+        if n%x == 0:
+            p = False
+    return p
+
+prime(17)
+prime()
