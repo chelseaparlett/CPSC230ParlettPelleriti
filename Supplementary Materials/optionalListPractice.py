e4e8e87e78eed864208b10dbee1b9bd2e58e1580
cmparlettpelleriti <parlett@chapman.edu>
2019-09-30 12:48:13 -0700

    optional List practice

---
+++ Supplementary Materials/optionalListPractice.py
@@ -0,0 +1,19 @@
+# Write some code that finds all the numbers divisible by
+# 8 and 14 and stores them in a list.
+#---YOUR CODE HERE---------------------------------------
+
+#--------------------------------------------------------
+
+# Write some code that asks a user for an item
+# and stores it in a list called groceries. Keep
+# asking them for an item until they type "stop".
+# Print out the list.
+#---YOUR CODE HERE---------------------------------------
+
+#--------------------------------------------------------
+
+# Write some code that loops (a for loop) through each string in the list
+# below and only prints out those that have exclusively numbers.
+#---YOUR CODE HERE---------------------------------------
+myList = ["hello123", "19348", "hweuh12", "13", "0", "green"]
+#--------------------------------------------------------
