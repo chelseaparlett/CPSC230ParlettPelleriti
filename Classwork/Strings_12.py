bd75b1e51e4ca0322892c91fbefcc343e6ba00df
cmparlettpelleriti <parlett@chapman.edu>
2021-09-10 15:37:39 -0700

    Strings 12

---
+++ Classwork/Strings_12.py
@@ -0,0 +1,72 @@
+# Class Activities + Review (Class 12)
+
+# Together------------------------------------------------
+
+# 0. Questions?
+# 1. String Methods
+# 2. Censor
+
+# Classwork-----------------------------------------------
+'''
+1. Let's do a new madlib program. Use the  string madlib and the
+.replace(old,new) method  to replace the ADJECTIVE, VERBS,NOUNS..etc
+in the string with user entered strings.
+'''
+
+#string.replace(old, new)
+
+madlib = '''Two *NOUN1*, both alike in dignity,
+In fair *PLACE1*, where we lay our scene,
+From ancient *NOUN2* break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-crossd *NOUN3* take their life;
+Whole misadventured piteous overthrows
+Do with their *NOUN4* bury their parents strife.
+The fearful passage of their *ADJECTIVE2* love,
+And the continuance of their parents rage,
+Which, but their childrens end, nought could *VERB1* ,
+Is now the *NUMBER* hours traffic of our stage;
+The which if you with *ADJECTIVE3* *BODYPART* attend,
+What here shall *VERB2*, our toil shall strive to mend.'''
+
+
+'''
+2. Write a program that asks the user who the host of Jeopardy was from 1984
+until 2020. Make sure to cont them as correct even if they use different
+capitalization, or if they have extra spaces in their answer. If they're
+correct print "Correct!" and finish the program. Otherwise ask them to
+answer again.
+'''
+
+'''
+3. Re-create and extend our password example from Classwork 7. Ask
+the user to enter a potential password, and check if it meets your critera.
+If their password does not meet the criteria, tell them why, and ask them
+for a new password. Continue to ask them for a new password if their old one
+is bad. Make sure each password:
+
+- is at least 10 characters long
+- is not "Password123"
+- contains at least one of the following characters: !@$%&
+- has at least one alphabetical character
+- has at least one number
+- is less than 50 characters long
+'''
+
+
+'''
+4. 133+ speak is a way of writing english using symbols to replace many english
+letters. Use python to change the string below into leet speak based on the
+replacements listed below. Make sure to account for capitals!
+
+a = @
+b = |3
+e = 3
+l = 1
+o = 0
+s = $
+t = +
+'''
+
+my_string = "Hello! I appear to be lost. Can someone direct me to the website Reddit? Bye!"
