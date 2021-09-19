2dc3722581609b7729de3b08b8a28c84a6fb98df
cmparlettpelleriti <parlett@chapman.edu>
2021-09-18 17:10:37 -0700

    15 record

--- Lectures/ListsAndTuples_15_BLANK.py
+++ Lectures/ListsAndTuples_15_BLANK.py
@@ -1,15 +1,6 @@
 # lists
 l = ["Tan", "Antoni", "Karamo", "Jonathan", "Bobby"]
 
-for fab in l:
-    print("I love", fab)
-
-x = [1,2,3,4,5,6]
-
-for i in x:
-    print(i**2)
-
-
 days = ["Monday",
 "Tuesday",
 "Wednesday",
@@ -18,18 +9,10 @@ days = ["Monday",
 "Saturday",
 "Sunday"]
 
-for day in days:
-    if day == "Tuesday":
-        print("it's taco time")
-
 # indexing
-print(days[0])
-print(l[2:4])
-print(l[::-1])
 
-# mutability
-l[0] = "Tim"
-print(l)
+# # mutability
+
 
 # nested lists
 spreadsheet = [["Name", "Age", "GPA"],
@@ -38,15 +21,10 @@ spreadsheet = [["Name", "Age", "GPA"],
 ["Tony", 23, 3.7]]
 
 ## how do I grab "Bill"?
-print(spreadsheet[3][2])
 
 
 # tuples
-my_tup = (1,2,3,4,5)
 
-print(my_tup[0])
-
-my_tup[0] = 0
 
 # looping through more complex lists
 
@@ -54,7 +32,3 @@ spreadsheet = [["Name", "Age", "GPA"],
 ["Bill", 7, 0.0],
 ["Chelsea", 24,  4.0],
 ["Tony", 23, 3.7]]
-
-for row in spreadsheet:
-    for cell in row:
-        print(cell)
