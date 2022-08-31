9db10bb08fdd011c75eb4d868bdf904c0cbb1234
cmparlettpelleriti <cparlett@chapman.edu>
2022-08-31 09:44:25 -0700

    added debugging examples

--- Classwork/IntroToCS_1.py
+++ Classwork/IntroToCS_1.py
@@ -1,29 +1,53 @@
 # Class Activities + Review (Class 1)
-
 # Together------------------------------------------------
 
 # 0. Quiz!
-# 1. Check that Atom is Working
-    # a. Atom
-    # b. Platformio IDE Terminal
+# 1. Check that VSCode is Working
 # 2. Do our own "Hello World"
 # 3. Run a Script
 # 4. Questions?
 
+# hello world
+
+
+# CPSC 230 is a great class (user input)
+
+
+# Syntax Errors
+print("Hello World"
+print("Chelsea)
+
 # Classwork-----------------------------------------------
-''' 1. In the lines below, write your own Hello World program
-and run it using the platformio package. If you'd like, you
-can also add a line that asks the user for their name, and
-then prints out Hello [Name] instead of Hello World.
+# ''' 1. In the lines below, write your own Hello World program
+# and run it using the Terminal. If you'd like, you
+# can also add a line that asks the user for their name, and
+# then prints out Hello [Name] instead of Hello World.
+#
 
-2. With your groupmates, translate the following numbers into
-binary:
 
-- 17
-- 16
-- 256
-- 400
-- 2
-- 1
-- 8
-'''
+
+# 2. With your groupmates, translate the following numbers into
+# binary:
+#
+# - 17
+# - 16
+# - 256
+# - 400
+# - 2
+# - 1
+# - 8
+# '''
+
+
+# 3. Can you figure out what's causing these syntax errors?
+# uncomment each one and run to see the error message
+
+##-------------------------------------------------------
+# favorite_class = input("What's your favorite class? "))
+
+##-------------------------------------------------------
+# test = 'My grandma's pie is my favorite pie'
+
+
+
+
