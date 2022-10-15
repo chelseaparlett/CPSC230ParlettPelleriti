fd39760f12094633af4920f53a55efd7593390f4
cmparlettpelleriti <parlett@chapman.edu>
2022-10-15 16:31:24 -0700

    updated review lec

--- Classwork/Functions_14.py
+++ Classwork/Functions_14.py
@@ -6,13 +6,30 @@
 # 1. Default Arguments
 # 2. Giving a function argument values
 
+# log function
+import math
+
+#quadratic
+
+
+# how could I just change b?
+# how could i give the arguments in a different order?
+
+# email function
+body = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.'''
+
+
 # Classwork-----------------------------------------------
 '''
-1. Write a function perfect_number() that takes in an integer, n and returns
-true if it's a perfect number, and False otherwise.
-
-Remember a perfect number is a number whose divisors (besides itself) add up
-to itself. E.g. 6 (1 + 2 + 3 = 6).
+1. Write a function, euclidean() that takes in 4 arguments, x1, x2, y1, and y2,
+and calculates (+ returns) the euclidean distance between the points (x1,y1) and (x2,y2).
+Make the default arguments 1 for all 4 arguments. 
 '''
 
 
@@ -26,7 +43,7 @@ Make sure to account for upper and lower cases (e.g. Kayak is still okay).
 
 '''
 3. Write a function squarer() that takes in a number, j, and prints out the
-squares of every number from 1 to j.
+squares of every number from 1 to j. 
 '''
 
 '''
@@ -76,4 +93,12 @@ add_tags('b', 'NO') -> '<b>NO</b>'
 instance of the word "you" (any capitalization like "you", OR "You") with "Nicholas Cage". Return
 the new string. And set the default value to be "I love you". Call your function
 using the defaults.
+'''
+
+'''
+10. Write a function, piglatin() that takes in a string, s, and returns the
+pig latin version of that word. 
+
+(use the same rules as Project 1).
+
 '''
